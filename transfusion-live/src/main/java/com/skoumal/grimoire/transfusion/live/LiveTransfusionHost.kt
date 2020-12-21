6686889b30ec1020f87ca9ff1331e8206ccd973d
Viktor De Pasquale <info@depasquale.wiki>
2020-12-21 14:01:32 +0100

    Added docs to live transfusion

--- transfusion-live/src/main/java/com/skoumal/grimoire/transfusion/live/LiveTransfusionHost.kt
+++ transfusion-live/src/main/java/com/skoumal/grimoire/transfusion/live/LiveTransfusionHost.kt
@@ -5,14 +5,37 @@ import androidx.lifecycle.Observer
 
 fun interface TransfusionObserver<T> : Observer<T>
 
+/**
+ * Host allowing managed consumption of [LiveData] inside non-lifecycle scope. By using this you're
+ * responsible for clearing the listeners as the whole object is cleared - that be with the usage of
+ * [AutoCloseable] or in a ViewModel's [onCleared][androidx.lifecycle.ViewModel.onCleared].
+ *
+ * You can use default implementation like so:
+ *
+ * ```kotlin
+ * class MyViewModel: ViewModel()
+ * ```
+ * */
 interface LiveTransfusionHost {
 
+    /**
+     * Starts a consumption on provided LiveData. Be wary that you absolutely **have to**
+     * [clearTransfusions] if you want to use LiveData inside non-lifecycle scope.
+     *
+     * @return Observer, that doesn't need to be saved or cached as the default implementation
+     * implements this functionality for you.
+     * */
     fun <T> LiveData<T>.transfusion(observer: TransfusionObserver<T>): Observer<T>
 
+    /** Clears all listeners and destroys references to all transfusions */
     fun clearTransfusions()
 
     companion object {
 
+        /**
+         * Creates and returns default implementation. All transfusions are saved internally and
+         * cleared completely when calling [clearTransfusions].
+         * */
         fun getDefault(): LiveTransfusionHost = DefaultLiveTransfusionHost()
 
     }
@@ -47,8 +70,8 @@ private class DefaultLiveTransfusionHost : LiveTransfusionHost {
     }
 
     private class TransfusionHolder<T>(
-        private val observable: LiveData<T>,
-        private val observer: Observer<T>
+        @JvmField private val observable: LiveData<T>,
+        @JvmField private val observer: Observer<T>
     ) {
 
         fun clear() = observable.removeObserver(observer)
